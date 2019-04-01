3b0c9159ead0215e3ff694820fd8f232cc5731eb
Arthur <verdon.arthur@gmail.com>
2019-04-01 15:12:20 +0200

    add multi lang

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <router-link to="/" class="navbar-item" href>Quizz proto</router-link>
+          <span class="navbar-burger burger" data-target="navbarMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenu" class="navbar-menu">
+          <div class="navbar-end">
+            <span class="navbar-item">
+              <router-link to="/about" class="button is-outlined">
+                <span class="icon">
+                  <i class="fa fa-home"></i>
+                </span>
+                <span>About</span>
+              </router-link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div>
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueI18n from 'vue-i18n'
+import { MessageFR } from './lang/fr.js'
+import i18n from './i18n'
+
+Vue.use(Buefy)
+Vue.use(VueI18n)
+console.log(MessageFR)
+const i18n = new VueI18n({
+  locale: 'fr', // set locale
+  MessageFR // set locale messages
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  i18n,
+  i18n
 }).$mount('#app')
